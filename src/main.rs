2cd88d96dbc51d5b930b0c7c982c1ed1b819533b
raphael_leurond <leurond.raphael@gmail.com>
2026-05-15 18:01:16 +0200

    feature: implement first functionnal version of minigrep search tool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use minigrep::search;
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -10,13 +12,20 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
 }
 
 struct Config {
